50eb3672f9f65473f617c758e8ef82c82d9b2738
Guilherme Andrade <guilhermeandrrade14@gmail.com>
2026-08-18 14:19:05 -0300

    Initialize list and display first/last numbers
    Create a list of numbers and print details.

---
+++ treinando_listas.py
@@ -0,0 +1,19 @@
+# Criando Lista
+
+numeros = []
+
+numeros.append("1")
+numeros.append("2")
+numeros.append("3")
+numeros.append("4")
+numeros.append("5")
+
+primeiro_numero = numeros[0]
+ultimo_numero = numeros[4]
+print(numeros)
+
+print(f"Números existentes: {len(numeros)}.")
+
+print(f"Primeiro número {primeiro_numero}")
+
+print(f"Último número: {ultimo_numero}")
